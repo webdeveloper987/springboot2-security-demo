2378ca6f62da053b308c88be78a4f042932d6500
web.developer987 <webdeveloper987@users.noreply.github.com>
2018-03-05 09:59:36 +0100

    minor

--- src/main/java/com/springboot2/security/demosecurity/DemoSecurityApplication.java
+++ src/main/java/com/springboot2/security/demosecurity/DemoSecurityApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoSecurityApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(DemoSecurityApplication.class, args);
